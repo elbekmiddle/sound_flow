a5c1942d0576c72d175b24506edee763dbe6e671
ELbek Rustamjanov <elbek4561@gmail.com>
2026-05-26 21:18:34 +0500

    auto install yt-dlp via postinstall

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -2,9 +2,6 @@
 # Exit on error
 set -o errexit
 
-echo "Installing Node dependencies..."
-npm install
-
 echo "Downloading yt-dlp..."
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
 chmod a+rx yt-dlp
